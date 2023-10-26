313ed7df2da67f65361909f550aeb79a2b24e336
lisorius <148572778+lisorius@users.noreply.github.com>
2023-10-26 21:19:11 +0300

    Create html_file.py

---
+++ html_file.py
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<p style="margin-top:400px">
+    <body style = 'text-align: center' >
+<style>
+    body{background: aquamarine}
+    .class2{
+     border-radius: 20%;
+        background: forestgreen;
+        width: 400px;
+        height: 40px;
+        position: absolute; top: 25%; left: 50%;
+        transform: translate(-50%, -50%);"
+
+    }
+</style>
+<style>
+.class1{
+
+border-radius: 20%;
+        background: cadetblue;
+        width: 400px;
+        height: 155px;
+        position: absolute; top: 40%; left: 50%;
+        transform: translate(-50%, -50%);"
+
+}
+</style>
+
+
+
+
+
+<div>
+<h2 class="class2">Пройдите авторизацию:</h2>
+</div>
+<div ip = 'div1' class="class1">
+<h4> Логин:<input></h4>
+<h4> Пароль:<input></h4>
+<button style="border-radius: 35%; width: 220px; height: 40px; background-color: blue; color: black;">Войти</button>
+    </div>
+</body></p>
+</html>
